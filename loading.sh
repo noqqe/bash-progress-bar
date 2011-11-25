570d36be1e06b961cf628fdfc1d3f667523acda7
Florian Baumann <flo@noqqe.de>
2011-11-25 15:04:29 +0100

    init

---
+++ loading.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+MAX=${1:-11}
+TIME="${2:-0.08}"
+TL="${3:-[}"
+S="${4:-#####}"
+TR="${5:-]}"
+
+
+while true; do
+    R=0
+    while [ $R -lt $MAX ]; do 
+        clear
+        RSP=$(($MAX - $R ))
+        if [ $RSP -gt $MAX ]; then RSP=$MAX ; fi 
+        LSP=$(($MAX - ${RSP}))
+        echo -n $TL
+        for l in $(seq 1 $LSP); do
+            echo -n " "
+        done
+        echo -n $S
+        for r in $(seq 1 $RSP); do
+            echo -n " "
+        done; echo $TR
+        sleep $TIME ; ((R++))
+    done
+    while [ $R -ne 0 ]; do
+        clear
+        RSP=$(($MAX - $R ))
+        if [ $RSP -ge $MAX ]; then RSP=$MAX ; fi 
+        LSP=$(($R + 0 )) 
+        if [ $LSP -lt 0 ]; then LSP=0 ; fi 
+        echo -n $TL
+        for l in $(seq 1 $R); do
+            echo -n " "
+        done
+        echo -n $S
+        for r in $(seq 1 $RSP); do
+            echo -n " "
+        done; echo $TR
+        sleep $TIME; clear ; ((R--))
+    done
+done
